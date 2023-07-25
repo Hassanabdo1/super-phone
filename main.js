7b9e9c1639a2e43980fe86f8e732d21ff6db5318
Hassanabdo1 <137386378+Hassanabdo1@users.noreply.github.com>
2023-07-25 16:24:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let img = document.querySelector('.imege');
+let contanr = document.querySelector('.containatr');
+
+
+
+function phones(phone){
+img.src = phone;
+}
+
+function colors(color){
+    contanr.style.background = color;
+
+
+}
